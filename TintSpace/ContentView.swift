c4af43c61b686677d0400226f9237d03491e81be
SAI DUTT <duttganduri46@gmail.com>
2025-03-13 18:49:28 -0700

    Initial Commit

---
+++ TintSpace/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TintSpace
+//
+//  Created by Sai Dutt Ganduri on 3/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TintSpace/TintSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TintSpaceApp.swift
+//  TintSpace
+//
+//  Created by Sai Dutt Ganduri on 3/13/25.
+//
+
+import SwiftUI
+
+@main
+struct TintSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
